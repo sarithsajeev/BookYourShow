db948a79d9e30d5dc4395aae8bd9510f83c8c732
Anjali Nair <anjali.nair@experionglobal.com>
2021-12-29 20:00:59 -0800

    showtime api added

---
+++ Controllers/ShowTimeController.cs
@@ -0,0 +1,97 @@
+﻿using BookYourShow.Models;
+using BookYourShow.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookYourShow.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShowTimeController : ControllerBase
+    {
+        IShowTimeRepo showtimeRepository;
+
+        public ShowTimeController(IShowTimeRepo _p)
+        {
+            showtimeRepository = _p;
+        }
+        [HttpGet]
+        [ProducesResponseType(typeof(ShowTime), 200)]
+        [ProducesResponseType(404)]
+
+
+        public async Task<IActionResult> GetShowTime()
+        {
+
+            var showtime = await showtimeRepository.GetShowTime();
+            if (showtime == null)
+            {
+                return NotFound();
+            }
+            return Ok(showtime);
+
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(ShowTime), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> AddShowTime(ShowTime showtime)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+
+                var resId = await showtimeRepository.AddShowTime(showtime);
+                if (resId != null)
+                {
+                    return Ok(resId);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPut]
+
+        [ProducesResponseType(typeof(ShowTime), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> UpdateShowTime([FromBody] ShowTime model)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+
+                await showtimeRepository.UpdateShowTime(model);
+                return Ok();
+
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ShowTime), 200)]
+        [ProducesResponseType(404)]
+        //[TypeFilter(typeof(CustomExceptionFilter))]
+        public async Task<IActionResult> GetShowTimeById(int id)
+        {
+            //throw new Exception("New Exception");
+            var showtime = await showtimeRepository.GetShowTimeById(id);
+            if (showtime == null)
+            {
+                return NotFound();
+            }
+            return Ok(showtime);
+        }
+
+    }
+}
--- Repository/IShowTimeRepo.cs
+++ Repository/IShowTimeRepo.cs
@@ -1,11 +1,30 @@
-﻿using System;
+﻿using BookYourShow.Models;
+using BookYourShow.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookYourShow.Repository
 {
-    interface IShowTimeRepo
+     public interface IShowTimeRepo
     {
+        /// <summary>
+        /// insert showtime
+        /// </summary>
+        /// <param name="showTime"></param>
+        /// <returns></returns>
+        Task<ShowTime> AddShowTime(ShowTime showTime);
+
+
+        /// <summary>
+        /// display showtime 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<ShowTimeView>> GetShowTime();
+
+        Task<ShowTimeView> GetShowTimeById(int id);
+
+        Task UpdateShowTime(ShowTime showtime);
     }
 }
