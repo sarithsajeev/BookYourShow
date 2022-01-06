20846e52b8cf08c204f58e9fbe34b5df362cea43
Roshni A T <roshni.at@experionglobal.local>
2022-01-06 16:17:01 +0530

    seat changed

--- src/BookYourShow.Api/Controllers/SeatController.cs
+++ src/BookYourShow.Api/Controllers/SeatController.cs
@@ -1,5 +1,5 @@
 ﻿using BookYourShow.Models;
-using BookYourShow.Repository;
+using BookYourShow.Api.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
--- src/BookYourShow.Api/Repository/SeatRepository.cs
+++ src/BookYourShow.Api/Repository/SeatRepository.cs
@@ -1,11 +1,12 @@
-﻿using BookYourShow.Models;
+﻿using BookYourShow.Api.Repository;
+using BookYourShow.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookYourShow.Repository
+namespace BookYourShow.Api.Repository
 {
     public class SeatRepository:ISeatRepository
     {
