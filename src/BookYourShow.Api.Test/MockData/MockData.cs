c4335d62e58350754dc3a4b39c651c56735310aa
Avin Joseph <avin.joseph@experionglobal.com>
2022-01-10 11:13:08 +0530

    made changes

--- src/BookYourShow.Api.Test/MockData/MockData.cs
+++ src/BookYourShow.Api.Test/MockData/MockData.cs
@@ -3,7 +3,7 @@ using BookYourShowAPI.Repositories;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BookYourShow.Api.Test.MockData
@@ -12,19 +12,21 @@ namespace BookYourShow.Api.Test.MockData
     {
         public static async Task<List<Theatre>> GetTheatre()
         {
-          var _theatres = new List<Theatre>()
-          { 
-            new Theatre()
-            { 
-                TheatreId =10,
-                TheatreName = "Pluto Ciniplex",
-                Location="Gayatripuram",
-                CityId=1,
-                IsActive=true
-            }
-          };
+            var _theatres = new List<Theatre>()
+                {
+                new Theatre()
+                    {
+                        TheatreId =10,
+                        TheatreName = "Pluto Ciniplex",
+                        Location="Gayatripuram",
+                        CityId=1,
+                        IsActive=true
+                    }
+                };
             return _theatres;
         }
+
+
 
         public static Mock<ITheatreRepo> GetTheatreByIdMock()
         {
@@ -37,32 +39,36 @@ namespace BookYourShow.Api.Test.MockData
                 IsActive = true
             };
 
+
+
             var mockRepo = new Mock<ITheatreRepo>();
             mockRepo.Setup(r => r.GetTheatreById(1)).ReturnsAsync(_theatre);
             return mockRepo;
         }
 
+
+
         public static Mock<ITheatreRepo> AddTheatreRepoMock()
         {
-          var _theatres = new List<Theatre>()
-          {
-           new Theatre()
-           {
-                TheatreId =11,
-                TheatreName = "PLR Ciniplex",
-                Location="lakshmipuram",
-                CityId=1,
-                IsActive=true
-           },
-           new Theatre()
-           {
-                TheatreId =12,
-                TheatreName = "BMT Ciniplex",
-                Location="Vijayanagar",
-                CityId=2,
-                IsActive=true
-           },
-         };
+            var _theatres = new List<Theatre>()
+            {
+            new Theatre()
+            {
+            TheatreId =11,
+            TheatreName = "PLR Ciniplex",
+            Location="lakshmipuram",
+            CityId=1,
+            IsActive=true
+            },
+            new Theatre()
+            {
+            TheatreId =12,
+            TheatreName = "BMT Ciniplex",
+            Location="Vijayanagar",
+            CityId=2,
+            IsActive=true
+            },
+            };
             var mockRepo = new Mock<ITheatreRepo>();
             mockRepo.Setup(r => r.AddTheatre(It.IsAny<Theatre>())).ReturnsAsync((Theatre theatre) =>
             {
@@ -71,6 +77,8 @@ namespace BookYourShow.Api.Test.MockData
             });
             return mockRepo;
         }
+
+
 
         public static Mock<ITheatreRepo> UpdateTheatreMock()
         {
@@ -82,6 +90,8 @@ namespace BookYourShow.Api.Test.MockData
                 CityId = 2,
                 IsActive = true
             };
+
+
 
             var mockRepo = new Mock<ITheatreRepo>();
             mockRepo.Setup(r => r.UpdateTheatre(It.IsAny<Theatre>())).ReturnsAsync(true);
